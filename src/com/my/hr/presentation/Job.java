b7423aaf0f56092a75de74e818c03ad7203ebab4
'국종석' <'a@a.com'>
2023-01-17 15:10:21 +0900

    update

--- src/com/my/hr/presentation/Job.java
+++ src/com/my/hr/presentation/Job.java
@@ -1,7 +1,7 @@
 package com.my.hr.presentation;
 
 public enum Job {
-	// 객체지향으로 표현하면 "회원"이 덧붙어야 한다. ex. 회원목록, 회원추가...
+	// 객체지향으로 표현하면 "회원"이 덧붙어야 한다. ex. 회원목록, 회원추가, ...
 	// 하지만 중복을 제거하기 위해 "회원"을 뺐다. trade-off
 	EXIT("종료"), LIST("목록"), ADD("추가"), FIX("수정"), DEL("삭제");
 	
